6a9d13fa58892c2bcea29bb35e2ee1346e003ada
Harry Palma <harrypalma@MacBook-Pro-de-Moofwd.local>
2024-12-11 20:12:35 -0300

    Deploying

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -xe
 
+# Ensure the 'tomcat' user and group exist
+if ! grep -q "^tomcat:" /etc/group; then
+    groupadd tomcat
+fi
+
+if ! id -u tomcat &>/dev/null; then
+    useradd -r -g tomcat tomcat
+fi
 
 # Copy war file from S3 bucket to tomcat webapp folder
 aws s3 cp s3://codedeploystack2-webappdeploymentbucket-hampbdlbbgtw/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
-
-# Ensure the ownership permissions are correct.
+# Ensure the ownership permissions are correct
 chown -R tomcat:tomcat /usr/local/tomcat9/webapps
